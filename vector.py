53b7d4eda2fd6d69a705aed1c96be8db878ee0c4
slink007 <slink_1@cox.net>
2021-06-10 10:17:55 -0400

    basic vector addition

--- vector.py
+++ vector.py
@@ -20,3 +20,17 @@ class Vector(object):
     def __eq__(self, v):
         return self.coordinates == v.coordinates
 
+
+    def add(self, v):
+        """
+        Adds one Vector to another Vector and returns a tuple containing the result.
+        
+        Returning a Vector is better option?  
+        
+        Currently this assumes both Vectors are of same size (problem?) and that both are
+        two-dimensional Vectors (definitely a problem).
+        """
+        if not isinstance(v, Vector):
+            raise TypeError('Tried to add a non-vector')
+
+        return (self.coordinates[0] + v.coordinates[0], self.coordinates[1] + v.coordinates[1])
