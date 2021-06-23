ac71ea8f4892ff4ec4b9db612535276bbb93e259
slink007 <slink_1@cox.net>
2021-06-23 11:49:00 -0400

    Reverted line.py back to the original version because I've learned the hard way that this version needs work before you can even create an instance of Line.  Once I've corrected the instructor provided code I will move on to adding methods to it.

--- line.py
+++ line.py
@@ -10,15 +10,6 @@ class Line(object):
     NO_NONZERO_ELTS_FOUND_MSG = 'No nonzero elements found'
 
     def __init__(self, normal_vector=None, constant_term=None):
-        """
-        Expects a line in the form of Ax + By = k.
-
-        The normal_vector input needs to be an iterable suitable for forming
-        a Vector.  The values in that iterable must be the 'A' and 'B' 
-        values from the line equation.
-
-        Constant term = k. 
-        """
         self.dimension = 2
 
         if not normal_vector:
@@ -53,11 +44,7 @@ class Line(object):
 
 
     def __str__(self):
-    """
-    Creates the string that is printed out when a Line object is
-    printed.  For example, if the Line is 3x - 2y = 1 this will
-    print 3x_1 - 2x_2 = 1.
-    """
+
         num_decimal_places = 3
 
         def write_coefficient(coefficient, is_initial_term=False):
@@ -109,63 +96,6 @@ class Line(object):
                 return k
         raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
 
-
-    def is_parallel(self, l):
-        """
-        Return True if Line l is parallel to this Line, otherwise return False.
-        """
-        v1 = Vector(self.normal_vector)
-        v2 = Vector(l.normal_vector)
-
-        return v1.is_parallel(v2)
-
-
-    def is_same(self, l):
-        """
-        Returns True if this Line and Line l describe the same line.  Returns False
-        if that is not the case.
-        """
-        ratio = self.normal_vector[0] / l.normal_vector[0]
-        try:
-            for i in range(1, len(self.normal_vector)):
-                diff = MyDecimal( abs((self.normal_vector[i] / l.normal_vector[i]) - ratio) )
-                assert diff.is_near_zero()
-        except AssertionError:
-            return False
-        return True
-
-
-    def intersection(self, l):
-        """
-        Returns a tuple indicating how many intersections there are between two Lines.
-        If lines are parallel, and there are no intersections, it returns (0,).
-        If lines are parallel, and the lines are the same, it returns (-1,).
-        If lines are not parallel it calculates the intersection point and returns the
-        coordinates of that intersection as (x, y).
-        """
-        if not isinstance(l, Line):
-            raise TypeError('Intersection test requires a line.')
-
-        if is_parallel(l):
-            if is_same(l):
-                return (-1,)
-            return (0,)
-
-        # If we got here then the lines are not parallel and they have an intersection at
-        # (x, y) with:
-        # x = (DK1 - BK2) / (AD - BC)
-        # y = (AK2 - CK1) / (AD - BC)
-        A = self.normal_vector[0]
-        B = self.normal_vector[1]
-        K1 = self.constant_term
-        C = l.normal_vector[0]
-        D = l.normal_vector[1]
-        K2 = l.constant_term
-        denominator = (A * D) - (B * C)
-        x = ((D * K1) - (B * K2)) / denominator
-        y = ((A * K2) - (C * K1)) / denominator
-
-        return (x, y)
 
 class MyDecimal(Decimal):
     def is_near_zero(self, eps=1e-10):
