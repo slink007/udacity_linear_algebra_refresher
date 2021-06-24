faa7392503fae2f28fa024b54c508ae2b3bf6e2a
slink007 <slink_1@cox.net>
2021-06-23 21:26:42 -0400

    Removed usage of Digit class from the Line class as it caused me more problems than it solved.  Removing Digit keeps my Line class compatible with my Vector class.
    Added, and tested, _is_parallel and _is_same to the Line class.

--- line.py
+++ line.py
@@ -1,8 +1,8 @@
-from decimal import Decimal, getcontext
+#from decimal import Decimal, getcontext
 
 from vector import Vector
 
-getcontext().prec = 30
+#getcontext().prec = 30
 
 
 class Line(object):
@@ -18,8 +18,10 @@ class Line(object):
         self.normal_vector = normal_vector
 
         if not constant_term:
-            constant_term = Decimal(0)
-        self.constant_term = Decimal(constant_term)
+            constant_term = 0
+            #constant_term = Decimal(0)
+        self.constant_term = constant_term
+        #self.constant_term = Decimal(constant_term)
 
         self.set_basepoint()
 
@@ -77,7 +79,8 @@ class Line(object):
             basepoint_coords = [0]*self.dimension
 
             initial_index = Line.first_nonzero_index(n)
-            initial_coefficient = Decimal(n[initial_index])
+            initial_coefficient = n[initial_index]
+            #initial_coefficient = Decimal(n[initial_index])
             # Failure to cast as Decimal on previous line was causing script
             # to crash.  Can't divide Decimal by a float.
 
@@ -104,25 +107,39 @@ class Line(object):
         Return True if Line l is the same line as this Line, otherwise return False.
         """
 
-        # lines must be parallel to be the same line
-        if not self._is_parallel(l):
-            return False
-
         # The Vector between the two basepoints
         basepoint_difference = self.basepoint.subtract(l.basepoint)
 
+        # If the vector is orthogonal then these lines are the same.
         return basepoint_difference.is_orthogonal(self.normal_vector)
+
+
+    def intersection(self, l):
+        """
+        Returns a tuple indicating how the lines do, or do not, intersect.
+        If the lines are the same return (-1,).
+        If the lines are parallel, but not the same, return (0,).
+        If the lines intersect return a tuple of the x and y coordinates of the 
+        intersection point.
+        """
+        if self._is_parallel(l):
+            if self._is_same(l):
+                return (-1,)
+            return (0,)
+
+        # TODO: Put the intersecting point code in here
 
 
     @staticmethod
     def first_nonzero_index(iterable):
         for k, item in enumerate(iterable):
-            if not MyDecimal(item).is_near_zero():
+            #if not MyDecimal(item).is_near_zero():
+            if not (round(item, 9) == 0):
                 return k
         raise Exception(Line.NO_NONZERO_ELTS_FOUND_MSG)
 
 
 
-class MyDecimal(Decimal):
-    def is_near_zero(self, eps=1e-10):
-        return abs(self) < eps
+#class MyDecimal(Decimal):
+#    def is_near_zero(self, eps=1e-10):
+#        return abs(self) < eps
